dc60f7bc3395735209657ab8a9cf4a160b315083
thommie-wallin <thommie.wallin@chasacademy.se>
2023-12-17 19:36:41 +0100

    setup of test environment

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import './App.css';
-import { useWeatherData } from './api/api.js';
-import Today from './components/Today';
-import WeekOverview from './components/WeekOverview';
-import Hourly from './components/Hourly';
-import WeekForecast from './components/WeekForecast';
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import "./App.css";
+import { useWeatherData } from "./api/api.js";
+import Today from "./components/Today";
+import WeekOverview from "./components/WeekOverview";
+import Hourly from "./components/Hourly";
+import WeekForecast from "./components/WeekForecast";
 
-function App() {  
-  const {weatherData} = useWeatherData();
+function App() {
+  const { weatherData } = useWeatherData();
   const [isTempUnit, setIsTempUnit] = useState(true);
 
   // Toggle temperature unit
   const toggleUnit = () => {
-    setIsTempUnit(
-      isTempUnit ? false : true
-    )
-  }
+    setIsTempUnit(isTempUnit ? false : true);
+  };
 
   return (
     <Router>
@@ -25,7 +23,7 @@ function App() {
           <div className="title">
             <h1>u09 Weather App </h1>
             <button onClick={toggleUnit}>
-              Change {isTempUnit ? '℃' : '℉'}
+              Change {isTempUnit ? "℃" : "℉"}
             </button>
           </div>
           <div className="timezone">
@@ -34,22 +32,42 @@ function App() {
         </header>
         <nav className="navbar">
           <ul className="navList">
-            <li><Link to="/">Today</Link></li>
-            <li><Link to="/hourly">Hourly</Link></li>
-            <li><Link to="/fiveday">5 day</Link></li>
+            <li>
+              <Link to="/">Today</Link>
+            </li>
+            <li>
+              <Link to="/hourly">Hourly</Link>
+            </li>
+            <li>
+              <Link to="/fiveday">5 day</Link>
+            </li>
           </ul>
         </nav>
         <div className="router-content">
           <Switch>
             <Route exact path="/">
-              {Object.keys(weatherData).length > 0 && <Today weatherData={weatherData} isTempUnit={isTempUnit} />}
-              {Object.keys(weatherData).length > 0 && <WeekOverview weatherData={weatherData} isTempUnit={isTempUnit} />}
+              {Object.keys(weatherData).length > 0 && (
+                <Today weatherData={weatherData} isTempUnit={isTempUnit} />
+              )}
+              {Object.keys(weatherData).length > 0 && (
+                <WeekOverview
+                  weatherData={weatherData}
+                  isTempUnit={isTempUnit}
+                />
+              )}
             </Route>
             <Route path="/hourly">
-              {Object.keys(weatherData).length > 0 && <Hourly weatherData={weatherData} isTempUnit={isTempUnit} />}
+              {Object.keys(weatherData).length > 0 && (
+                <Hourly weatherData={weatherData} isTempUnit={isTempUnit} />
+              )}
             </Route>
             <Route path="/fiveday">
-              {Object.keys(weatherData).length > 0 && <WeekForecast weatherData={weatherData} isTempUnit={isTempUnit} />}
+              {Object.keys(weatherData).length > 0 && (
+                <WeekForecast
+                  weatherData={weatherData}
+                  isTempUnit={isTempUnit}
+                />
+              )}
             </Route>
           </Switch>
         </div>
--- src/api/api.js
+++ src/api/api.js
@@ -1,19 +1,24 @@
-import { useState, useEffect } from 'react';
-import { useGeolocation } from './geolocation';
+import { useState, useEffect } from "react";
+import { useGeolocation } from "./geolocation";
+import fakeData from "../apiCall.json";
 
 export const useWeatherData = () => {
-  const {latitude, longitude, error} = useGeolocation();
-  const [weatherData, setWeatherData] = useState({});
+  const { latitude, longitude, error } = useGeolocation();
+  // const [weatherData, setWeatherData] = useState({});
+  const [weatherData, setWeatherData] = useState(fakeData);
   const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
-  const parameter = 'alert,minutely';
+  const parameter = "alert,minutely";
 
   useEffect(() => {
     if (latitude && longitude) {
-      fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=${parameter}&appid=${apiKey}`)
-        .then(response => response.json())
-        .then(data => setWeatherData(data));
+      fetch(
+        `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=${parameter}&appid=${apiKey}`
+      )
+        .then((response) => response.json())
+        // .then((data) => setWeatherData(data));
+        .then((data) => console.log(data));
     }
-  }, [latitude, longitude, apiKey])
+  }, [latitude, longitude, apiKey]);
 
-  return {weatherData, error};
-}
+  return { weatherData, error };
+};
