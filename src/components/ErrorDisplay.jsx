3a9f2cff591d083502afd65682cb27fe8351a157
thommie-wallin <thommie.wallin@chasacademy.se>
2024-01-31 11:42:30 +0100

    create errorDisplay card

--- src/components/ErrorDisplay.jsx
+++ src/components/ErrorDisplay.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import { useForecastContext } from "../services/contexts/forecast-context";
+import Card from "./ui/Card";
 
 const ErrorDisplay = () => {
   const { error } = useForecastContext();
   return (
-    <div>
-      <p>
-        Something went wrong! Please try again. Error:{" "}
-        {JSON.stringify(error.error.message)}
-      </p>
-    </div>
+    <Card title={"Something went wrong! Please try again."}>
+      <p className="error-text">Error: {JSON.stringify(error.error.message)}</p>
+    </Card>
   );
 };
 
