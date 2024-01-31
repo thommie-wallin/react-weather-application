dd68d3c95d0bddf8f219d762978ff408b5d8a57e
thommie-wallin <thommie.wallin@chasacademy.se>
2024-01-31 18:06:19 +0100

    install React and React-dom 18.2

--- src/hooks/useGetGeolocationPosition.jsx
+++ src/hooks/useGetGeolocationPosition.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { getGeolocationPosition } from "../services/geolocation";
 import { useForecastContext } from "../services/contexts/forecast-context";
 import { useNavigate } from "react-router-dom";
@@ -8,26 +8,33 @@ const useGetGeolocationPosition = () => {
     useForecastContext();
   const navigate = useNavigate();
 
+  // Clean up useEffect to make hook only run once.
+  const geolocationRef = useRef(true);
+
   // If permitted, get user position from Geolocation API after first render.
   useEffect(() => {
-    navigator.permissions.query({ name: "geolocation" }).then(async (res) => {
-      if (res.state === "granted") {
-        loadingStart();
-        try {
-          const posObj = await getGeolocationPosition();
-          setPosition({
-            latitude: posObj.coords.latitude,
-            longitude: posObj.coords.longitude,
-          });
-        } catch (error) {
-          setError(error);
-        } finally {
-          loadingStop();
+    if (geolocationRef.current) {
+      geolocationRef.current = false;
+
+      navigator.permissions.query({ name: "geolocation" }).then(async (res) => {
+        if (res.state === "granted") {
+          loadingStart();
+          try {
+            const posObj = await getGeolocationPosition();
+            setPosition({
+              latitude: posObj.coords.latitude,
+              longitude: posObj.coords.longitude,
+            });
+          } catch (error) {
+            setError(error);
+          } finally {
+            loadingStop();
+          }
+        } else if (res.state === "denied") {
+          navigate("/");
         }
-      } else if (res.state === "denied") {
-        navigate("/");
-      }
-    });
+      });
+    }
   }, []);
 };
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ForecastProvider } from "./services/contexts/forecast-context";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <ForecastProvider>
       <App />
     </ForecastProvider>
   </React.StrictMode>,
-  document.getElementById("root"),
 );
 
 // If you want to start measuring performance in your app, pass a function
