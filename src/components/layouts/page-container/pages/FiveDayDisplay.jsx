c6541ca2a7341d1007dcfbe5f21191c933ab2cfe
thommie-wallin <thommie.wallin@chasacademy.se>
2024-02-13 23:09:56 +0100

    add tbody-element in HourlyDisplay.jsx and FiveDayDisplay.jsx to correct semantic html

--- src/components/layouts/page-container/pages/FiveDayDisplay.jsx
+++ src/components/layouts/page-container/pages/FiveDayDisplay.jsx
@@ -4,18 +4,23 @@ import ForecastIcon from "../../../ui/ForecastIcon";
 const FiveDayDisplay = ({ fiveDayforecast }) => {
   return (
     <table className="list-forecast">
-      {fiveDayforecast.map((d, i) => (
-        <tr key={i} className="forecast-list-item">
-          <td>{d.dates}</td>
-          <td className="forecast-list-temp">{d.temps}°</td>
-          <td>
-            <ForecastIcon iconName={d.iconsName} iconAlt={d.mainDescription} />
-          </td>
-          <td className="forecast-list-description">{d.description}</td>
-          <td>{d.windSpeed}m/s</td>
-          <td>{d.humidity}%</td>
-        </tr>
-      ))}
+      <tbody>
+        {fiveDayforecast.map((d, i) => (
+          <tr key={i} className="forecast-list-item">
+            <td>{d.dates}</td>
+            <td className="forecast-list-temp">{d.temps}°</td>
+            <td>
+              <ForecastIcon
+                iconName={d.iconsName}
+                iconAlt={d.mainDescription}
+              />
+            </td>
+            <td className="forecast-list-description">{d.description}</td>
+            <td>{d.windSpeed}m/s</td>
+            <td>{d.humidity}%</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 };
--- src/components/layouts/page-container/pages/HourlyDisplay.jsx
+++ src/components/layouts/page-container/pages/HourlyDisplay.jsx
@@ -4,18 +4,23 @@ import ForecastIcon from "../../../ui/ForecastIcon";
 const HourlyDisplay = ({ hourlyData }) => {
   return (
     <table className="list-hourly">
-      {hourlyData.map((d, i) => (
-        <tr key={i} className="hourly-list-item">
-          <td>{d.hour}</td>
-          <td className="hourly-list-temp">{d.temperature}°</td>
-          <td>
-            <ForecastIcon iconName={d.iconsName} iconAlt={d.mainDescription} />
-          </td>
-          <td className="hourly-list-description">{d.description}</td>
-          <td>{d.windSpeed}m/s</td>
-          <td>{d.humidity}%</td>
-        </tr>
-      ))}
+      <tbody>
+        {hourlyData.map((d, i) => (
+          <tr key={i} className="hourly-list-item">
+            <td>{d.hour}</td>
+            <td className="hourly-list-temp">{d.temperature}°</td>
+            <td>
+              <ForecastIcon
+                iconName={d.iconsName}
+                iconAlt={d.mainDescription}
+              />
+            </td>
+            <td className="hourly-list-description">{d.description}</td>
+            <td>{d.windSpeed}m/s</td>
+            <td>{d.humidity}%</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 };
