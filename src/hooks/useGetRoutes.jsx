3717a45bdfa729362489b9b7bdf90fdfa91a248b
thommie-wallin <thommie.wallin@chasacademy.se>
2024-02-13 17:11:14 +0100

    implement code splitting with lazy loading the import of each page-component in react-router-dom, use react component-functions lazy and Suspense

--- src/hooks/useGetRoutes.jsx
+++ src/hooks/useGetRoutes.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Route } from "react-router-dom";
 import { routes } from "../router/routes";
 import Root from "../pages";
+import LoadingDisplay from "../components/LoadingDisplay";
+import { PageContainer } from "../components/layouts/page-container/PageContainer";
 
 const useGetRoutes = () => {
   return (
     <Route path="/" element={<Root />}>
       {routes.map(({ path, element }, i) => (
-        <Route key={i} path={path} element={element} />
+        <Route
+          key={i}
+          path={path}
+          element={
+            <Suspense
+              fallback={
+                <PageContainer>
+                  <LoadingDisplay />
+                </PageContainer>
+              }
+            >
+              {element}
+            </Suspense>
+          }
+        />
       ))}
     </Route>
   );
--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,8 +1,14 @@
-import React from "react";
-import TodayPage from "../pages/dashboard/today";
-import HourlyPage from "../pages/dashboard/hourly";
-import FiveDayPage from "../pages/dashboard/five-day";
-import RadarPage from "../pages/dashboard/radar";
+import React, { lazy, Suspense } from "react";
+
+const TodayPage = lazy(() => import("../pages/dashboard/today"));
+const HourlyPage = lazy(() => import("../pages/dashboard/hourly"));
+const FiveDayPage = lazy(() => import("../pages/dashboard/five-day"));
+const RadarPage = lazy(() => import("../pages/dashboard/radar"));
+
+// import TodayPage from "../pages/dashboard/today";
+// import HourlyPage from "../pages/dashboard/hourly";
+// import FiveDayPage from "../pages/dashboard/five-day";
+// import RadarPage from "../pages/dashboard/radar";
 
 export const routes = [
   {
